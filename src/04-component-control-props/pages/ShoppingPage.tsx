180737dd5c07333a963fa7a90424c9c13988fdd5
Juan Aguirre <j1927aguirre@gmail.com>
2023-02-12 16:54:39 -0500

    Add initial steps for shopping cart

--- src/04-component-control-props/pages/ShoppingPage.tsx
+++ src/04-component-control-props/pages/ShoppingPage.tsx
@@ -1,6 +1,8 @@
 import { ProductCard, ProductImage, ProductTitle, ProductButtons } from '../components';
+import { Product } from '../interfaces/interfaces';
 import '../styles/custom-styles.css';
-const product = {
+
+const product1 = {
   id: '1',
   title: 'Coffee Mug',
   image: './coffee-mug.png'
@@ -11,6 +13,8 @@ const product2 = {
   image: './coffee-mug2.png'
 };
 
+const products: Product[] = [product1, product2];
+
 export const ShoppingPage = () => {
   return (
     <div>
@@ -20,23 +24,42 @@ export const ShoppingPage = () => {
         display: 'flex',
         flexWrap: 'wrap'
       }}>
+        {
+          products.map(product => (
+
+            <ProductCard
+              className="bg-dark text-white"
+              product={product}>
+              <ProductImage className="custom-image" style={{
+                boxShadow: '10px 10px 10px rgba(0, 0, 0, 0.2)'
+              }} />
+              <ProductTitle className="text-bold" />
+              <ProductButtons className="custom-buttons" />
+            </ProductCard>
+          ))
+        }
+      </div>
+      <div className="shopping-cart">
         <ProductCard
+          style={{ width: '100px' }}
           className="bg-dark text-white"
-          product={product}>
-          <ProductCard.Image className="custom-image" />
-          <ProductCard.Title className="text-bold" />
-          <ProductCard.Buttons className="custom-buttons" />
+          product={product1}>
+          <ProductImage className="custom-image" style={{
+            boxShadow: '10px 10px 10px rgba(0, 0, 0, 0.2)'
+          }} />
+          <ProductButtons className="custom-buttons" />
         </ProductCard>
         <ProductCard
+          style={{ width: '100px' }}
           className="bg-dark text-white"
           product={product2}>
           <ProductImage className="custom-image" style={{
             boxShadow: '10px 10px 10px rgba(0, 0, 0, 0.2)'
-          }}/>
-          <ProductTitle className="text-bold" />
+          }} />
           <ProductButtons className="custom-buttons" />
         </ProductCard>
       </div>
+
     </div>
   );
 };
