644e3d0ee07327f73f19290560e9a9d789958b3b
Juan Aguirre <j1927aguirre@gmail.com>
2023-03-05 11:13:26 -0500

    Add register form with submit event

--- src/07-formik-react-forms/pages/RegisterPage.tsx
+++ src/07-formik-react-forms/pages/RegisterPage.tsx
@@ -1,28 +1,60 @@
+import { ChangeEvent, FormEvent, useState } from 'react';
 import '../styles/styles.css';
 
-
 export const RegisterPage = () => {
+
+  const [registerData, setRegisterData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const { name, email, password, confirmPassword } = registerData;
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setRegisterData(prev => ({ ...prev, [name]: value }));
+  };
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(registerData);
+  };
+
   return (
     <div>
       <h1>Register Page</h1>
-      <form>
+      <form noValidate onSubmit={onSubmit}>
         <input
           type="text"
           placeholder="Name"
+          name="name"
+          value={name}
+          onChange={onChange}
         />
         <input
           type="email"
           placeholder="Email"
+          name="email"
+          value={email}
+          onChange={onChange}
         />
         <input
           type="password"
           placeholder="Password"
+          name="password"
+          value={password}
+          onChange={onChange}
         />
         <input
           type="password"
           placeholder="Confirm your password"
+          name="confirmPassword"
+          value={confirmPassword}
+          onChange={onChange}
         />
-        <button type="submit">Create</button>
+        <button type="submit">CREATE</button>
       </form>
     </div>
   );
