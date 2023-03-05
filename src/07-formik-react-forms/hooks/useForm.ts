6f4a4dc8fb875a2c14e0c96d959e51b9e14f2737
Juan Aguirre <j1927aguirre@gmail.com>
2023-03-05 11:27:08 -0500

    Add useForm custom hook

---
+++ src/07-formik-react-forms/hooks/useForm.ts
@@ -0,0 +1,16 @@
+import { ChangeEvent, useState } from 'react';
+
+export const useForm = <T>(initState: T) => {
+  const [formData, setFormData] = useState(initState);
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData(prev => ({ ...prev, [name]: value }));
+  };
+
+  return {
+    ...formData,
+    formData,
+    onChange,
+  };
+};
